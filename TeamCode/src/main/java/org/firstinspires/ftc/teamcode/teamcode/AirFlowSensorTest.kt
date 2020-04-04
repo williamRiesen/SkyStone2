0c9745847df09ac35b0de123ef9283d0cd4cece0
Bill Riesen <riesenw@mercyhealth.com>
2020-04-03 20:46:57 -0500

    Airflow sensor returns actual airflow data

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/AirFlowSensorTest.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/AirFlowSensorTest.kt
@@ -1,13 +1,10 @@
 package org.firstinspires.ftc.teamcode.teamcode
 
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
-import com.qualcomm.robotcore.hardware.DeviceInterfaceModule
 import com.qualcomm.robotcore.hardware.I2cAddr
 import com.qualcomm.robotcore.hardware.I2cDeviceSynch
-import com.qualcomm.robotcore.hardware.I2cDeviceSynchSimple
 import com.qualcomm.robotcore.util.ElapsedTime
 
 
@@ -17,28 +14,26 @@ class AirFlowSensorTest : LinearOpMode() {
     private val runtime = ElapsedTime()
 
 
-
     override fun runOpMode() {
 //        val airflowSensor = hardwareMap.get (I2cDeviceSynchSimple::class.java, "airflow_sensor")
-        val airflowSensor = hardwareMap.get (I2cDeviceSynch::class.java, "airflow_sensor")
-        Thread.sleep(100)
+
+        val airflowSensor = hardwareMap.get(I2cDeviceSynch::class.java, "airflow_sensor")
         airflowSensor.engage()
         val manufacturerAddress = I2cAddr.create7bit(0x49)
         airflowSensor.i2cAddress = manufacturerAddress
-        telemetry.addData("isEngaged", airflowSensor.isEngaged)
-        telemetry.addData("isArmed", airflowSensor.isArmed)
-        telemetry.addData("7-bit address:  ", airflowSensor.i2cAddress)
-        telemetry.update()
         waitForStart()
         while (opModeIsActive()) {
-
-            telemetry.addData("reading", airflowSensor.read8(1))
+            val reading = airflowSensor.read(0, 3)
+            val byteOne = String.format("%02X", reading[0])
+            val byteTwo = String.format("%02X", reading[1])
+            val byteThree = String.format("%02X", reading[2])
+            telemetry.addData("byteOne", byteOne)
+            telemetry.addData("byteTwo", byteTwo)
+            telemetry.addData("byteThree", byteThree)
             telemetry.update()
+            Thread.sleep(20)
         }
-        Thread.sleep(5000)
-
     }
-
 
 
 }
